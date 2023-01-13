9177127f735451bdb6b69bdbd955ca87f653a650
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2023-01-13 14:55:26 +0900

    20230113-4

--- js/sub1.js
+++ js/sub1.js
@@ -1,5 +1,13 @@
+// 헤더 슬라이드 다운/업 헤더 호버 시 글씨 색 바꾸기
+$(document).on('mouseenter','.topmenu',function(){
+    $(this).children(".hiddenmenu").stop().slideDown(400).css({"display":"block"});
+}).on('mouseleave','.topmenu',function(){
+    $(this).children(".hiddenmenu").stop().slideUp(400).css({"display":"none"});
+});
 $(function(){
-    $('header>ul>li').on("click",function(){
-        $(this).siblings('.hiddenmenu').slideToggle();
+    $(".hiddenmenu").children("li").hover(function(){
+        $(this).parent(".hiddenmenu").siblings("a").css({"color":"#5cc8d8"});
+    },function(){
+        $(".topmenu").children("a").css({"color":"white"});
     });
 });
