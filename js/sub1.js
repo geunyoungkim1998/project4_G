9795ffa50b4ed762929c565dd9bb4ae2afcc0a1b
kim geun young <119376320+geunyoungkim1998@users.noreply.github.com>
2023-02-03 15:38:48 +0900

    20230203

--- js/sub1.js
+++ js/sub1.js
@@ -13,6 +13,11 @@ function fnSlide() {
     $("img[alt='slide']:first-child").insertAfter("img[alt='slide']:last-child");
   });
 };
+$("#sec2slide img").click(function(){
+  var imgIndex=$("#sec2slide img").index(this);
+  var arr=["https://www.youtube.com/embed/bVbyn7c1X6E","https://www.youtube.com/embed/G16-w-hMKTU","https://www.youtube.com/embed/h7XSEqWGxGI","https://www.youtube.com/embed/5ybulMdPLKY","https://www.youtube.com/embed/A35X4h590Ek","https://www.youtube.com/embed/15ESUxxJvZs","https://www.youtube.com/embed/eligdVVedDU"];
+  $("#slideIframe").attr("src", arr[imgIndex]);
+});
 
 // 캐릭터 슬라이드
 setInterval(fnSlide2, 5000);
